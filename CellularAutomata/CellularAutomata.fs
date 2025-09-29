27a9609b61091be860d6d82d75ee50b4cf22e5b7
rccarlson <rileyccarlson97@gmail.com>
2025-09-29 13:39:34 -0400

    Rename ruleset -> statetransform

--- CellularAutomata/CellularAutomata.fs
+++ CellularAutomata/CellularAutomata.fs
@@ -7,21 +7,21 @@ open System.Text
 type Board<'TState> = 'TState array2d
 
 /// Given a neighborhood and current state, return a new state
-type RuleSet<'TState,'TNeighborhood> = 'TState -> 'TNeighborhood -> 'TState
+type StateTransform<'TState,'TNeighborhood> = 'TState -> 'TNeighborhood -> 'TState
 
 let createBoard = Array2D.init
 
 /// Creates the next board based on the provided rule and neighborhood function.
 let getNextBoard
   (board:Board<'TState>)
-  (rules: RuleSet<'TState,'TNeighborhood>)
+  (getNextState: StateTransform<'TState,'TNeighborhood>)
   (getNeighborhood: int -> int -> 'TNeighborhood)
   : Board<'TState> =
     let width, height = Array2D.length1 board, Array2D.length2 board
     let getNextState x y : 'TState =
       let currentValue : 'TState = board.[x,y]
       let neighborhood : 'TNeighborhood = getNeighborhood x y
-      rules currentValue neighborhood
+      getNextState currentValue neighborhood
     createBoard width height getNextState
 
 let printBoard (board:Board<'TState>) (toChar : 'TState -> char) =
@@ -29,11 +29,10 @@ let printBoard (board:Board<'TState>) (toChar : 'TState -> char) =
   let sb = StringBuilder()
   for y in 0 .. height - 1 do
     for x in 0 .. width - 1 do
-      sb.Append(toChar board[x,y]) |> ignore
+      sb.Append(toChar board.[x,y]) |> ignore
     sb.AppendLine() |> ignore
   Console.Write(sb.ToString())
 
-  
 /// Gets the Moore neighborhood (8 surrounding cells) of a cell at (x,y)
 let getMooreNeighborhood (board:Board<'TState>) x y : 'TState array =
   let width, height = board.GetLength(0), board.GetLength(1)
